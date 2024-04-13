5b89474777c01f867035863ee064da7c31ade394
Samuel BAGATTIN <42091952+SamuelBagattin@users.noreply.github.com>
2024-04-13 15:54:59 +0200

    ADd tags, prs and environments

--- modules/github_actions_assumable_role/main.tf
+++ modules/github_actions_assumable_role/main.tf
@@ -29,12 +29,12 @@ data "aws_iam_policy_document" "this" {
       variable = "token.actions.githubusercontent.com:sub"
     }
     condition {
-      test     = "ForAllValues:StringEquals"
+      test     = "StringEquals"
       values   = ["sts.amazonaws.com"]
       variable = "token.actions.githubusercontent.com:aud"
     }
     condition {
-      test     = "ForAllValues:StringEquals"
+      test     = "StringEquals"
       values   = ["https://token.actions.githubusercontent.com"]
       variable = "token.actions.githubusercontent.com:iss"
     }
